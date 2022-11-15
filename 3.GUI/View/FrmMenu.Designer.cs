c37f509161be0da818ce6033362cd2225b41f96e
hieu200341 <109935541+hieu200341@users.noreply.github.com>
2022-11-15 23:45:31 +0700

    Design Menu
    Design Menu

---
+++ 3.GUI/View/FrmMenu.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace _3.GUI.View
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMenu));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btn_lienhe = new System.Windows.Forms.Button();
+            this.btn_sanpham = new System.Windows.Forms.Button();
+            this.btn_nhanvien = new System.Windows.Forms.Button();
+            this.btn_khachhang = new System.Windows.Forms.Button();
+            this.btn_hoadon = new System.Windows.Forms.Button();
+            this.btn_buy = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelHome = new System.Windows.Forms.Panel();
+            this.lb_home = new System.Windows.Forms.Label();
+            this.panelmanchinh = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            this.panelHome.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.panelMenu.Controls.Add(this.btn_lienhe);
+            this.panelMenu.Controls.Add(this.btn_sanpham);
+            this.panelMenu.Controls.Add(this.btn_nhanvien);
+            this.panelMenu.Controls.Add(this.btn_khachhang);
+            this.panelMenu.Controls.Add(this.btn_hoadon);
+            this.panelMenu.Controls.Add(this.btn_buy);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 594);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // btn_lienhe
+            // 
+            this.btn_lienhe.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_lienhe.FlatAppearance.BorderSize = 0;
+            this.btn_lienhe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_lienhe.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btn_lienhe.Image = ((System.Drawing.Image)(resources.GetObject("btn_lienhe.Image")));
+            this.btn_lienhe.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_lienhe.Location = new System.Drawing.Point(0, 380);
+            this.btn_lienhe.Name = "btn_lienhe";
+            this.btn_lienhe.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btn_lienhe.Size = new System.Drawing.Size(220, 60);
+            this.btn_lienhe.TabIndex = 5;
+            this.btn_lienhe.Text = "  Liên Hệ";
+            this.btn_lienhe.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_lienhe.UseVisualStyleBackColor = true;
+            this.btn_lienhe.Click += new System.EventHandler(this.btn_lienhe_Click);
+            // 
+            // btn_sanpham
+            // 
+            this.btn_sanpham.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_sanpham.FlatAppearance.BorderSize = 0;
+            this.btn_sanpham.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_sanpham.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btn_sanpham.Image = ((System.Drawing.Image)(resources.GetObject("btn_sanpham.Image")));
+            this.btn_sanpham.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_sanpham.Location = new System.Drawing.Point(0, 320);
+            this.btn_sanpham.Name = "btn_sanpham";
+            this.btn_sanpham.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btn_sanpham.Size = new System.Drawing.Size(220, 60);
+            this.btn_sanpham.TabIndex = 4;
+            this.btn_sanpham.Text = "  Sản Phẩm";
+            this.btn_sanpham.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_sanpham.UseVisualStyleBackColor = true;
+            this.btn_sanpham.Click += new System.EventHandler(this.btn_sanpham_Click);
+            // 
+            // btn_nhanvien
+            // 
+            this.btn_nhanvien.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_nhanvien.FlatAppearance.BorderSize = 0;
+            this.btn_nhanvien.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_nhanvien.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btn_nhanvien.Image = ((System.Drawing.Image)(resources.GetObject("btn_nhanvien.Image")));
+            this.btn_nhanvien.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_nhanvien.Location = new System.Drawing.Point(0, 260);
+            this.btn_nhanvien.Name = "btn_nhanvien";
+            this.btn_nhanvien.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btn_nhanvien.Size = new System.Drawing.Size(220, 60);
+            this.btn_nhanvien.TabIndex = 3;
+            this.btn_nhanvien.Text = "  Nhân Viên";
+            this.btn_nhanvien.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_nhanvien.UseVisualStyleBackColor = true;
+            this.btn_nhanvien.Click += new System.EventHandler(this.btn_nhanvien_Click);
+            // 
+            // btn_khachhang
+            // 
+            this.btn_khachhang.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_khachhang.FlatAppearance.BorderSize = 0;
+            this.btn_khachhang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_khachhang.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btn_khachhang.Image = ((System.Drawing.Image)(resources.GetObject("btn_khachhang.Image")));
+            this.btn_khachhang.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_khachhang.Location = new System.Drawing.Point(0, 200);
+            this.btn_khachhang.Name = "btn_khachhang";
+            this.btn_khachhang.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btn_khachhang.Size = new System.Drawing.Size(220, 60);
+            this.btn_khachhang.TabIndex = 2;
+            this.btn_khachhang.Text = "  Khách Hàng";
+            this.btn_khachhang.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_khachhang.UseVisualStyleBackColor = true;
+            this.btn_khachhang.Click += new System.EventHandler(this.btn_khachhang_Click);
+            // 
+            // btn_hoadon
+            // 
+            this.btn_hoadon.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_hoadon.FlatAppearance.BorderSize = 0;
+            this.btn_hoadon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_hoadon.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btn_hoadon.Image = ((System.Drawing.Image)(resources.GetObject("btn_hoadon.Image")));
+            this.btn_hoadon.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_hoadon.Location = new System.Drawing.Point(0, 140);
+            this.btn_hoadon.Name = "btn_hoadon";
+            this.btn_hoadon.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btn_hoadon.Size = new System.Drawing.Size(220, 60);
+            this.btn_hoadon.TabIndex = 1;
+            this.btn_hoadon.Text = "  Hóa Đơn";
+            this.btn_hoadon.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_hoadon.UseVisualStyleBackColor = true;
+            this.btn_hoadon.Click += new System.EventHandler(this.btn_hoadon_Click);
+            // 
+            // btn_buy
+            // 
+            this.btn_buy.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_buy.FlatAppearance.BorderSize = 0;
+            this.btn_buy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_buy.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btn_buy.Image = ((System.Drawing.Image)(resources.GetObject("btn_buy.Image")));
+            this.btn_buy.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_buy.Location = new System.Drawing.Point(0, 80);
+            this.btn_buy.Name = "btn_buy";
+            this.btn_buy.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btn_buy.Size = new System.Drawing.Size(220, 60);
+            this.btn_buy.TabIndex = 0;
+            this.btn_buy.Text = "  Mua Hàng";
+            this.btn_buy.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_buy.UseVisualStyleBackColor = true;
+            this.btn_buy.Click += new System.EventHandler(this.btn_buy_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.panelLogo.Controls.Add(this.label2);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.panelLogo.Size = new System.Drawing.Size(220, 80);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(15, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "QUẢN LÍ BÁN HÀNG";
+            // 
+            // panelHome
+            // 
+            this.panelHome.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panelHome.Controls.Add(this.lb_home);
+            this.panelHome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHome.Location = new System.Drawing.Point(220, 0);
+            this.panelHome.Name = "panelHome";
+            this.panelHome.Size = new System.Drawing.Size(1273, 80);
+            this.panelHome.TabIndex = 0;
+            // 
+            // lb_home
+            // 
+            this.lb_home.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lb_home.AutoSize = true;
+            this.lb_home.Font = new System.Drawing.Font("Montserrat", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lb_home.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lb_home.Location = new System.Drawing.Point(592, 21);
+            this.lb_home.Name = "lb_home";
+            this.lb_home.Size = new System.Drawing.Size(106, 37);
+            this.lb_home.TabIndex = 0;
+            this.lb_home.Text = "HOME";
+            // 
+            // panelmanchinh
+            // 
+            this.panelmanchinh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelmanchinh.Location = new System.Drawing.Point(220, 80);
+            this.panelmanchinh.Name = "panelmanchinh";
+            this.panelmanchinh.Size = new System.Drawing.Size(1273, 514);
+            this.panelmanchinh.TabIndex = 1;
+            // 
+            // FrmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1493, 594);
+            this.Controls.Add(this.panelmanchinh);
+            this.Controls.Add(this.panelHome);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "FrmMenu";
+            this.Text = "FrmMenu";
+            this.Load += new System.EventHandler(this.FrmMenu_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
+            this.panelHome.ResumeLayout(false);
+            this.panelHome.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panelMenu;
+        private Button btn_lienhe;
+        private Button btn_sanpham;
+        private Button btn_nhanvien;
+        private Button btn_khachhang;
+        private Button btn_hoadon;
+        private Button btn_buy;
+        private Panel panelLogo;
+        private Panel panelHome;
+        private Label lb_home;
+        private Label label2;
+        private Panel panelmanchinh;
+    }
+}
---
+++ 3.GUI/View/FrmMenu.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3.GUI.View
+{
+    public partial class FrmMenu : Form
+    {
+        private Button currentButton;
+        private Random random;
+        private int tempIndex;
+        private Form activeForm;
+        public FrmMenu()
+        {
+            InitializeComponent();
+            random = new Random();
+            //btnCloseChildForm.Visible = false;
+            this.Text = string.Empty;
+            this.ControlBox = false;
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
+        }
+
+        private Color SelectThemeColor()
+        {
+            int index = random.Next(ThemeColor.ColorList.Count);
+            while (tempIndex == index)
+            {
+                index = random.Next(ThemeColor.ColorList.Count);
+            }
+            tempIndex = index;
+            string color = ThemeColor.ColorList[index];
+            return ColorTranslator.FromHtml(color);
+        }
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = SelectThemeColor();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    panelHome.BackColor = color;
+                    panelLogo.BackColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                    ThemeColor.PrimaryColor = color;
+                    ThemeColor.SecondaryColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                    //btnCloseChildForm.Visible = true;
+                }
+            }
+        }
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            } 
+            ActivateButton(btnSender);
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.panelmanchinh.Controls.Add(childForm);
+            this.panelmanchinh.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            lb_home.Text = childForm.Text;
+        }
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(51, 51, 76);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                    previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+
+        private void btn_buy_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btn_hoadon_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btn_khachhang_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btn_nhanvien_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btn_sanpham_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btn_lienhe_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new View.FrmLienHe(), sender);
+        }
+
+        private void FrmMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
