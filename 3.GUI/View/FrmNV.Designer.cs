41639c9a438b09520f5e2271bb605c002045cf4a
daoqpqp <109519223+daoqpqp@users.noreply.github.com>
2022-12-07 22:41:42 +0700

    Đạo

--- 3.GUI/View/FrmNV.Designer.cs
+++ 3.GUI/View/FrmNV.Designer.cs
@@ -366,7 +366,6 @@
             this.button1.TabIndex = 35;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // FrmNV
             // 
