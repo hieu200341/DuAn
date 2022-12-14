a9a5a00bfb38efd1b1e0a680edc5370ec5a4a96e
hieu200341 <109935541+hieu200341@users.noreply.github.com>
2022-12-14 22:28:04 +0700

    hieu

--- 3.GUI/Properties/Resources.Designer.cs
+++ 3.GUI/Properties/Resources.Designer.cs
@@ -263,6 +263,16 @@ namespace _3.GUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icons8_repeat_304 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-repeat-304", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icons8_search_32 {
             get {
                 object obj = ResourceManager.GetObject("icons8-search-32", resourceCulture);
